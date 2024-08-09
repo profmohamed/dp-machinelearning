490b1e056c9c6f70686b548d4cc57d33bcddbf80
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 08:38:28 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,7 @@ with st.expander('Data'):
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x ='bill_length_mm', y='body_mass_g', color= 'species')
+
+# Data preparations
+with str.sidebar():
+  str.header('Input feature')
