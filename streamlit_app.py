d081f9d3ccbcc9e8e97ebe4b3c44e968c9dd9830
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 06:31:51 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+import pandas as pd
 
+st.title(' 🤖 Machine Learning App ')
+st.info('this app builds  a machine learning model ')
+
+df = pd.read_scv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
