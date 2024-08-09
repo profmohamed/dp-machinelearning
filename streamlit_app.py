2b4a5d33ba8ef897db89b7c794ef8756e3077791
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 14:51:53 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
-prediction_proba
+df_prediction_proba
   
   
 
