5579730ba6de3bde6d80e8bc4d1eca84018cbc28
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 06:41:46 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ import pandas as pd
 st.title(' 🤖 Machine Learning App ')
 st.info('this app builds  a machine learning model ')
 
-df = pd.read_scv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write ('**Raw data**')
+  df = pd.read_scv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
