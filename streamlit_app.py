01615741f12c2d42d2ffbc026d7bc4dccbe32ee1
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 14:59:27 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,9 +82,13 @@ prediction_proba = clf.predict_proba(input_row)
 
 df_prediction_proba = pd.DataFrame(prediction_proba)
 df_prediction_proba.columns = ['Adelie','Chinstrap','Gentoo']
-df_prediction_proba
-  
-  
+
+#df_prediction_proba
+
+# Display Predicted species
+st.subheader('Predicted Species')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.success(str(penguins_species[prediction][0]))
 
 
 
