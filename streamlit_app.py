98193a980ee8a6354cc014a9fa8c0b74251866d0
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 06:50:36 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ with st.expander('Data'):
   st.write ('**Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+  st.write('**x**')
+  x = df.drop('species', axis= 1)
+  x
+  st.write('**y**')
+  y = df.species
+  y
