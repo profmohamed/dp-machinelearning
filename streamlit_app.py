8fc7fc427a97bd429c3dfa54751cfdd80b30ca5f
mohamed salem <mohamedsalim2051@gmail.com>
2024-08-09 08:42:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x ='bill_length_mm', y='body_mass_g', color= 'species')
 
 # Data preparations
-with st.sidebar():
+with st.sidebar:
   st.header('Input features')
   st.footer(' Made with love by mohamed salem ')
